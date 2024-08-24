ac5b8478b5abace9dbadec4447ca0628606cfd32
Gaurav0111 <garwalgaurav1@gmail.com>
2024-08-24 10:39:12 +0530

    minor

--- backend/dropboxService.js
+++ backend/dropboxService.js
@@ -1,7 +1,8 @@
+require('dotenv').config();
 const Dropbox = require('dropbox').Dropbox;
 const fetch = require('isomorphic-fetch');
 
-const dbx = new Dropbox({ accessToken: 'sl.B7ieH2iRqBEbwEC9jIK5K5szWFSr0ZNo-LTDc2xyOVQeSBb3ny2fF5zDzg-Sxn2lTHB_FCEXOFmCjMOU3Sqq8VTEN-rgSr-_HU02MqB9A3qdoYycPSzOvMbN-DDskKc_Xxl0_Qm0a3T6', fetch: fetch });
+const dbx = new Dropbox({ accessToken: process.env.DROPBOX_TOKEN, fetch: fetch });
 
 const uploadFileToDropbox = async (filePath, fileContent) => {
     try {
