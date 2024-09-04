637e5dff4f98af8ff1a9c55bf65cb5d867165a4c
Gaurav0111 <garwalgaurav1@gmail.com>
2024-09-04 23:23:38 +0530

    completed login feature

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bcrypt = require('bcryptjs');
 const { check, validationResult } = require('express-validator');
 const User = require('../models/User');
+const jwt = require('jsonwebtoken');
 
 const router = express.Router();
 
