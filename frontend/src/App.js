d51a595876eb617319631ac3f23274f38a9abea6
Gaurav0111 <garwalgaurav1@gmail.com>
2024-09-11 23:36:08 +0530

    created error page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,16 +3,18 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './pages/LoginPage';
 import Register from './pages/RegisterPage';
 import Landing from './pages/LandingPage';
+import Error from './pages/Error';
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Landing />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/landing" element={<Landing />} />
+        <Route path="*" element={<Error />} />
       </Routes>
-    </Router>   
+    </Router>
   );
 }
 
