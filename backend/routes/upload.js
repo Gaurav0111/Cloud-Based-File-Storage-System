8c908c0d562d7ccdd8d0e6ded1b5394ee8366271
Gaurav0111 <garwalgaurav1@gmail.com>
2024-11-20 21:49:48 +0530

    final changes

--- backend/routes/upload.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { upload, uploadFile } = require('../controllers/uploadController');
-const router = express.Router();
-
-// Use the correct endpoint if you want /api/upload/file
-router.post('/file', upload.single('file'), uploadFile);
-
-module.exports = router;
---
+++ client/src/App.js
@@ -0,0 +1,35 @@
+import { Routes, Route } from 'react-router-dom'
+import ProtectedRoutes from './protected-routes/ProtectedRoutes'
+import Home from './pages/Home'
+import Sidebar from './components/Sidebar'
+import LoginDialog from './components/LoginDialog'
+import UploadMedia from './pages/UploadMedia'
+import YourDocs from './pages/YourDocs'
+import YourImages from './pages/YourImages'
+import YourVideos from './pages/YourVideos'
+import AccountInfo from './components/AccountInfo'
+import AccountEditDialog from './components/AccountInfoEdit'
+import SearchPage from './pages/SearchPage'
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/search' element={<SearchPage />} />
+        <Route path='/' element={<ProtectedRoutes />} >
+          <Route path='/upload' element={<UploadMedia />} />
+          <Route path='/your-docs' element={<YourDocs />} />
+          <Route path='/your-images' element={<YourImages />} />
+          <Route path='/your-videos' element={<YourVideos />} />
+        </Route>
+      </Routes>
+      <Sidebar />
+      <LoginDialog />
+      <AccountInfo />
+      <AccountEditDialog />
+    </>
+  )
+}
+
+export default App
